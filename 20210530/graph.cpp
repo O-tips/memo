711ca3fef753252cae4bd69802aaee53589de5fa
si-tm <kumomaya@gmail.com>
2021-05-30 19:29:37 +0900

    add 20210530

--- 20210530/graph.cpp
+++ 20210530/graph.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+//graph[3] = {4, 5, 1}
 vector<int> graph[1050];
+/*
+graph[1] = {3, 4}
+graph[2] = {}
+graph[3] = {}
+graph[4] = {1}
+graph[5] = {3}
+*/
+
 int main() {
-    graph[3].push_back(5);//町3から町5へ
-    graph[3].push_back(2);//町3から町2へ
-    graph[3].push_back(8);//町3から町8へ
-    int a = 3;
+    graph[1].push_back(4);//町1から町4への道が存在
+    graph[1].push_back(3);//町1から町3へ
+    graph[4].push_back(1);//町4から町1へ
+    graph[5].push_back(3);//町3から町8へ
+    int a = 1;
     for (int i = 0; i < graph[a].size(); i++) {
         //頂点aに連結しているすべての頂点を最小回数で洗い出せる
         cout << graph[a][i] << ' ';//5 2 8
